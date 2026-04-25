ae35ee32591f81e9748db886735bafe11af80995
Hussain Sheikh <sheikhhussein786@gmail.com>
2026-04-26 02:11:56 +0530

    spinner bug fix

--- frontend/app/components/TodoGrid.tsx
+++ frontend/app/components/TodoGrid.tsx
@@ -9,10 +9,13 @@ export default function TodoGrid({
   todos: Todo[];
   loading: boolean;
 }) {
-  if (loading) return <Spinner />;
   return (
     <div className="w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {todos.length === 0 ? (
+      {loading ? (
+        <div className="col-span-full flex justify-center py-16">
+          <Spinner />
+        </div>
+      ) : todos.length === 0 ? (
         <NoTasks />
       ) : (
         todos.map((todo: Todo) => <TodoCard key={todo.id} todo={todo} />)
